acf99a0eef4fbc89c142c0d070df484e0b941b4f
donboose <49300555+donboose@users.noreply.github.com>
2022-03-03 11:28:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("hello");
+
+function setup(){
+  createCanvas(600, 400);
+  background(200);
+}
+function draw(){
+  ellipse(50, 50, 80, 80);
+  fill(0);
+  if (mouseIsPressed){
+    fill(255);
+  } else {
+    fill(0);
+  }
+]
