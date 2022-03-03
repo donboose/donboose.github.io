c644182731bdfb3da50d3d206d8fd032f6bda559
donboose <49300555+donboose@users.noreply.github.com>
2022-03-03 18:32:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ function setup(){
 }
 
 let x = 10;
+let y = 450;
 let acc = 2
 
 function draw(){ 
-    background(200);
+    background(0, 255, 34);         // green background
 
-    fill(151, 244, 247);
-    rect(x, 200, 100, 20);
-    rect(x+15, 178, 70, 40);
-    fill(255);
-    ellipse(x+25, 221, 24, 24);
-    ellipse(x+75, 221, 24, 24);
+    fill(0, 43, 255);
+    rect(x, y, 45, 35);             // main body
+    rect(x+17.5, y-30, 10, 37.5);   // tank gun
+    fill(0);
+    rect(x-10, y-7.5, 12, 50);      // left wheel
+    rect(x+45, y-7.5, 12, 50);      // right wheel
+    fill(0, 43, 255);
+    rect(x+12.5, y+7.5, 20, 22);    // tank head
     
     if (keyIsPressed && keyCode === LEFT_ARROW){
         x = x - acc;
@@ -22,5 +25,19 @@ function draw(){
     if (keyIsPressed && keyCode === RIGHT_ARROW){
         x = x + acc;
     }
-    
+    if (keyIsPressed && keyCode === UP_ARROW){
+        y = y - acc;
+    } else if (keyIsPressed && keyCode === DOWN_ARROW){
+        y = y + acc;
+    }
+    if (x>980){
+        x = 20;
+    } else if (x<0){
+        x = 980;
+    }
+    if (y>480){
+        y = 20;
+    } else if (y<20){
+        y = 480;
+    }
 } 
