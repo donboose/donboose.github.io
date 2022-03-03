06ec9cfdce484ee77401256c322dd8dde5fbaf0c
donboose <49300555+donboose@users.noreply.github.com>
2022-03-03 20:31:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function setup(){
-    createCanvas(800, 400);
+    createCanvas(1250, 500);
     noStroke();
 }
 
@@ -19,6 +19,25 @@ function draw(){
     fill(0, 43, 255);
     rect(x+12.5, y+7.5, 20, 22);    // tank head
     
+    //buttons
+    fill(255);
+    rect(1160, 450, 40, 40);         //down
+    fill(0);
+    text("D", 1170, 460);
+    fill(255);
+    rect(1120, 450, 40, 40);         //right
+    fill(0);
+    text("R", 1130, 460);
+    fill(255);
+    rect(1160, 410, 40, 40);         //up
+    fill(0);
+    text("U", 1170, 420);
+    fill(255);
+    rect(1200, 450, 40, 40);         //left 
+    fill(0);
+    text("L", 1210, 460);
+    
+    //keyboard controls
     if (keyIsPressed && keyCode === LEFT_ARROW){
         x = x - acc;
     }
@@ -30,10 +49,23 @@ function draw(){
     } else if (keyIsPressed && keyCode === DOWN_ARROW){
         y = y + acc;
     }
-    if (x>980){
+    
+    //onscreen controls
+    if (mouseIsPressed && mouseX > 1160 && mouseX < 1200 && mouseY > 450 && mouseY < 490){
+        y = y + acc;
+    } else if (mouseIsPressed && mouseX > 1120 && mouseX < 1160 && mouseY > 450 && mouseY < 490){
+        x = x - acc;
+    } else if (mouseIsPressed && mouseX > 1160 && mouseX < 1200 && mouseY > 410 && mouseY < 450){
+        y = y - acc;
+    } else if (mouseIsPressed && mouseX > 1200 && mouseX < 1240 && mouseY > 450 && mouseY < 490){
+        x = x + acc;
+    }
+    
+    //edges
+    if (x>1230){
         x = 20;
     } else if (x<0){
-        x = 980;
+        x = 1230;
     }
     if (y>480){
         y = 20;
