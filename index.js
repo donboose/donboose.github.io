f9a239e7eb6cffd53813e97ee712f58806e627da
donboose <49300555+donboose@users.noreply.github.com>
2022-03-03 16:27:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-console.log("hello");
-
 function setup(){
-    createCanvas(600, 400);
-    background(200);
+    createCanvas(800, 400);
     noStroke();
 }
-function draw(){
-    ellipse(50, 50, 80, 80);
-    if (mouseIsPressed){
-        fill(100);
-    } else {
-        fill(255);
+
+let x = 10;
+let acc = 2
+
+function draw(){ 
+    background(200);
+
+    fill(151, 244, 247);
+    rect(x, 200, 100, 20);
+    rect(x+15, 178, 70, 40);
+    fill(255);
+    ellipse(x+25, 221, 24, 24);
+    ellipse(x+75, 221, 24, 24);
+    
+    if (keyIsPressed && keyCode === LEFT_ARROW){
+        x = x - acc;
     }
-}
+    if (keyIsPressed && keyCode === RIGHT_ARROW){
+        x = x + acc;
+    }
+    
+} 
