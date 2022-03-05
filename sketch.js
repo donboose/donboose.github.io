5ec4928a1514030163a6ca6dde36d09d5531e331
donboose <49300555+donboose@users.noreply.github.com>
2022-03-05 12:59:18 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let player;
+let enemies = [];
+let score = 0;
+function setup() {
+  createCanvas(1250, 500);
+  player = new Player();
+}
+
+function draw() {
+  background(0, 255, 34);
+  rectMode(CENTER);
+  drawReticle();
+  player.draw();
+  player.update();
+  
+  for (let enemy of enemies){
+      enemy.draw();
+      enemy.update();
+  }
+  if (frameCount % 200 == 0) {   // add this
+    enemies.push(new Enemy(1.5));
+  }
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    enemies[i].draw();
+    enemies[i].update();
+    if (enemies[i].hitYou()) { // add this
+        restart();
+        break;
+      }
+    if (player.hasShot(enemies[i])) {
+        score++;
+      enemies.splice(i, 1);
+    }
+  }
+  textAlign(CENTER);
+  textSize(40);
+  text(score, width/2, 100);
+  
+}
+function mouseClicked(){
+    player.shoot();
+}
+function restart() {
+    player = new Player();
+    enemies = [];
+    score = 0;
+  }
+  function drawReticle(){
+    fill(0, 43, 255);
+	ellipse(mouseX, mouseY, 20);
+	line(mouseX-14, mouseY-14, mouseX+14, mouseY+14);
+	line(mouseX+14, mouseY-14, mouseX-14, mouseY+14);
+	line(player.pos.x, player.pos.y, mouseX, mouseY);
+}
