65ed78ddb2712195dc4d4f86e6bcb8ebce1ca6dd
donboose <49300555+donboose@users.noreply.github.com>
2022-04-10 15:45:33 +0530

    Update bullet.js

--- bullet.js
+++ bullet.js
@@ -1,16 +1,17 @@
 class Bullet {
-    constructor(x, y, angle) {
+    constructor(x, y, angle, size, speed) {
       this.x = x;
       this.y = y;
       this.angle = angle;
-      this.speed = 16;
+      this.speed = speed;
+      this.size = size;
     }
     
     
     draw() {
       push();
       fill(0);
-      circle(this.x, this.y, 5);
+      circle(this.x, this.y, this.size);
       pop();
     }
     
